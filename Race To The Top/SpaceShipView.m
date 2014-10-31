557f69f1ef08a7f6aa2eebdeb79fee8d8846e495
BrainStarr <brian.r.starr@gmail.com>
2014-10-30 19:13:39 -0700

    initial setup

---
+++ Race To The Top/SpaceShipView.m
@@ -0,0 +1,21 @@
+//
+//  SpaceShipView.m
+//  Race To The Top
+//
+//  Created by Brian Starr on 10/30/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import "SpaceShipView.h"
+
+@implementation SpaceShipView
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+
+
+@end
