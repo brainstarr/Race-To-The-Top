bcc5a66552a6fa45e52925d10d2bd3b3bbfa94ca
BrainStarr <brian.r.starr@gmail.com>
2014-10-31 16:45:59 -0700

    added and implemented mountainPath class

---
+++ Race To The Top/MountainPath.h
@@ -0,0 +1,16 @@
+//
+//  MountainPath.h
+//  Race To The Top
+//
+//  Created by Brian Starr on 10/31/14.
+//  Copyright (c) 2014 Brian Starr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface MountainPath : NSObject
+
++(NSArray *)mountainPathsForRect:(CGRect)rect;
+
+@end
