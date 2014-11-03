fe349c6cd9b82225593433f0047c461d21959cc6
BrainStarr <brian.r.starr@gmail.com>
2014-11-03 12:56:49 -0800

    Still unable to connect PathView Subview to ViewController.m

--- Race To The Top/PathView.m
+++ Race To The Top/PathView.m
@@ -11,23 +11,31 @@
 
 @implementation PathView
 
-//- (id)initWithFrame:(CGRect)frame
-//{
-//    self = [super initWithFrame:frame];
-//    if (self) {
-//        // Initialization code
-//    }
-//    return self;
-//}
-//
-///* We override the method initWithCoder since this view will be initialized from the storyboard. Call the setup method to set the background color. */
-//-(id)initWithCoder:(NSCoder *)aDecoder
-//{
-//    self = [super initWithCoder:aDecoder];
-//    if (self){
-//    }
-//    return self;
-//}
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        [self setup];
+    }
+    return self;
+}
+
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self){
+        [self setup];
+    }
+    return self;
+}
+
+-(void)setup
+{
+    self.backgroundColor = [UIColor clearColor];
+}
+
+
 
 
 // Only override drawRect: if you perform custom drawing.
--- Race To The Top/ViewController.m
+++ Race To The Top/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "SpaceShipView.h"
+#import "PathView.h"
 
 @interface ViewController ()
+
+//@property (strong, nonatomic) IBOutlet PathView *pathView;
 
 @end
 
